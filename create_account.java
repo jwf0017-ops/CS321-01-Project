1d730672affaca7b499d968b286156b9151886f9
jorda <jhs0044@gmail.com>
2026-04-20 10:56:25 -0500

    create_account and login created and finished. Has not been tested - 04/20/26 JS

---
+++ create_account.java
@@ -0,0 +1,32 @@
+public class create_account{
+    public create_account(username,password) {
+        print("got the username and passcode")
+
+
+        //retive userlist
+        //place holder for now
+        int userlist = new int[100];
+        length = userlist.length
+        i = 0;
+        while (length < i) {
+            if (userlist[i] == username or userlost[ i] ==password)
+            {
+                print("dupicilate need a new username or passcode");
+                text = "need new username or passcode";
+                return text;
+
+
+            }
+            i += 1;
+
+
+        }
+    //now save using the function for userdatabase
+    }
+
+    print("username and passcode sucessfully saved");
+    }
+
+
+
+
---
+++ login_inter.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class login_inter {
+    public login(username, passcode) {
+        //assuming using name will be matched from the database
+        //assuming a function will get att the user IDS
+        username //going into function for userdatabase to get all atrbites
+        print("username is approved")
+
+    }
+}
